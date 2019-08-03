777c3c3eb293c3635c497060599b04fe9487dc4e
kevinqijie <15131547031@163.com>
2019-08-03 13:36:45 +0800

    ap2

--- src/main.js
+++ src/main.js
@@ -5,8 +5,10 @@ import store from './store'
 import axios from 'axios'
 
 Vue.prototype.axios = axios
-axios.defaults.timeout = 5000 // 请求超时
-axios.defaults.baseURL = 'http://39.97.33.178'
+// axios.defaults.timeout = 5000 // 请求超时
+// // eslint-disable-next-line no-unused-expressions
+// axios.defaults.headers['Content-Type']
+axios.defaults.baseURL = 'http://39.97.33.178/api'
 Vue.config.productionTip = false
 Vue.filter('setWH', (url, num) => {
   return url.replace(/w\.h/, num)
