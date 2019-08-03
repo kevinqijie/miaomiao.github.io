e73e535c61851d0fae4f67aec6ef756631eafe15
kevinqijie <15131547031@163.com>
2019-08-03 15:26:13 +0800

    ap2

--- src/main.js
+++ src/main.js
@@ -7,7 +7,9 @@ import axios from 'axios'
 Vue.prototype.axios = axios
 
 // axios.defaults.baseURL = '/api/'
-axios.defaults.baseURL = '/api'
+axios.defaults.baseURL = 'http://39.97.33.178'
+axios.defaults.headers.common['Authorization'] = 'AUTH_TOKEN'
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
 Vue.config.productionTip = false
 Vue.filter('setWH', (url, num) => {
   return url.replace(/w\.h/, num)
